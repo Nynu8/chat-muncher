fe67ef15eb395c4cbcdc7c4ba82be6113bf666b7
Adrian Szarnowski <adrianszarnowski8@gmail.com>
2021-07-09 21:17:25 +0200

    imp: dockerized the app

--- deploy.ps1
+++ deploy.ps1
@@ -2,5 +2,5 @@ cd SonataDiscordProxyBot
 dotnet publish -c release --runtime linux-arm --framework netcoreapp3.0
 cd 
 $Server = Read-Host -Prompt 'Input SSH server address: '
-scp bin\release\netcoreapp3.0\linux-arm\publish\* pi@${Server}:/home/pi/server/chat_muncher/
+scp bin\release\netcoreapp3.0\linux-arm\publish\* pi@${Server}:/home/pi/server/chat-muncher/src
 cd ..
